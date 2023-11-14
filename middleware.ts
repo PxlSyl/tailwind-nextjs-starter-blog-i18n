bb549c8f82a794241fc9c23bf7966093e76fc173
PxlSyl <SMCBeatz@protonmail.com.com>
2023-11-14 14:33:39 +0100

    up

--- middleware.ts
+++ middleware.ts
@@ -1,31 +1,25 @@
-import {NextResponse, NextRequest} from 'next/server';
-import { fallbackLng, locales } from 'app/[locale]/i18n/settings';
+import { NextResponse, NextRequest } from 'next/server'
+import { fallbackLng, locales } from 'app/[locale]/i18n/settings'
 
 export function middleware(request: NextRequest) {
   // Check if there is any supported locale in the pathname
-  const pathname = request.nextUrl.pathname;
+  const pathname = request.nextUrl.pathname
 
   // Check if the default locale is in the pathname
-  if (
-    pathname.startsWith(`/${fallbackLng}/`) ||
-    pathname === `/${fallbackLng}`
-  ) {
+  if (pathname.startsWith(`/${fallbackLng}/`) || pathname === `/${fallbackLng}`) {
     // e.g. incoming request is /en/about
     // The new URL is now /about
     return NextResponse.redirect(
       new URL(
-        pathname.replace(
-          `/${fallbackLng}`,
-          pathname === `/${fallbackLng}` ? '/' : '',
-        ),
-        request.url,
-      ),
-    );
+        pathname.replace(`/${fallbackLng}`, pathname === `/${fallbackLng}` ? '/' : ''),
+        request.url
+      )
+    )
   }
 
   const pathnameIsMissingLocale = locales.every(
-    locale => !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`,
-  );
+    (locale) => !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`
+  )
 
   if (pathnameIsMissingLocale) {
     // We are on the default locale
@@ -33,14 +27,13 @@ export function middleware(request: NextRequest) {
 
     // e.g. incoming request is /about
     // Tell Next.js it should pretend it's /en/about
-    return NextResponse.rewrite(
-      new URL(`/${fallbackLng}${pathname}`, request.url),
-    );
+    return NextResponse.rewrite(new URL(`/${fallbackLng}${pathname}`, request.url))
   }
 }
 
 export const config = {
   // Do not run the middleware on the following paths
+  // prettier-ignore
   matcher:
-  '/((?!api|static|.*\\..*|_next).*)',
-};
+  '/((?!api|static|.*\\..*|_next).*|favicon.ico)',
+}
