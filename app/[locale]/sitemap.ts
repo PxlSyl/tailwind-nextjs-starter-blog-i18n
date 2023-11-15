31b1aee88c6c396ec87600399246c1766f70e11a
PxlSyl <SMCBeatz@protonmail.com.com>
2023-11-15 17:36:03 +0100

    fix

--- app/[locale]/sitemap.ts
+++ app/[locale]/sitemap.ts
@@ -1,24 +1,34 @@
 import { MetadataRoute } from 'next'
 import { allBlogs } from 'contentlayer/generated'
 import siteMetadata from '@/data/siteMetadata'
+import { fallbackLng } from './i18n/settings'
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const siteUrl = siteMetadata.siteUrl
-  const blogRoutes = allBlogs.map((post) => ({
-    url: `${siteUrl}/${post.path}`,
-    lastModified: post.lastmod || post.date,
-  }))
-  const blogRoutesFR = allBlogs.map((post) => ({
-    url: `${siteUrl}/fr/${post.path}`,
-    lastModified: post.lastmod || post.date,
-  }))
-  const routes = ['', 'blog', 'projects', 'tags'].map((route) => ({
-    url: `${siteUrl}/${route}`,
+
+  const blogRoutes = allBlogs
+    .filter((p) => p.language === fallbackLng)
+    .map((post) => ({
+      url: `${siteUrl}/${fallbackLng}/${post.path}`,
+      lastModified: post.lastmod || post.date,
+    }))
+
+  const blogRoutesFR = allBlogs
+    .filter((p) => p.language === 'fr')
+    .map((post) => ({
+      url: `${siteUrl}/fr/${post.path}`,
+      lastModified: post.lastmod || post.date,
+    }))
+
+  const routes = ['', 'blog', 'projects', 'tags', 'about'].map((route) => ({
+    url: `${siteUrl}/${fallbackLng}/${route}`,
     lastModified: new Date().toISOString().split('T')[0],
   }))
-  const routesFR = ['', 'blog', 'projects', 'tags'].map((route) => ({
+
+  const routesFR = ['', 'blog', 'projects', 'tags', 'about'].map((route) => ({
     url: `${siteUrl}/fr/${route}`,
     lastModified: new Date().toISOString().split('T')[0],
   }))
+
   return [...routes, ...routesFR, ...blogRoutes, ...blogRoutesFR]
 }
