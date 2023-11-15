63d0efed05791de30663454cf7b3deb193fc50c2
PxlSyl <SMCBeatz@protonmail.com.com>
2023-11-16 00:13:23 +0100

    up

--- app/[locale]/i18n/settings.ts
+++ app/[locale]/i18n/settings.ts
@@ -1,10 +1,12 @@
 import type { InitOptions } from 'i18next'
 
 export const fallbackLng = 'en'
-export const locales = [fallbackLng, 'fr'] as const
+export const secondLng = 'fr'
+// define other languages here. Example :
+// export const thirdLng= 'es' and then add it to "locales" object
+export const locales = [fallbackLng, secondLng] as const
 export type LocaleTypes = (typeof locales)[number]
 export const defaultNS = 'common'
-export const cookieName = 'i18next'
 
 export function getOptions(locale = fallbackLng, ns = defaultNS): InitOptions {
   return {
--- app/[locale]/sitemap.ts
+++ app/[locale]/sitemap.ts
@@ -1,31 +1,32 @@
+//This file is the most important for SEO purposes so be sure to configure it perfectly with your project settings
 import { MetadataRoute } from 'next'
 import { allBlogs } from 'contentlayer/generated'
 import siteMetadata from '@/data/siteMetadata'
 import { fallbackLng } from './i18n/settings'
+import { secondLng } from './i18n/settings'
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const siteUrl = siteMetadata.siteUrl
-  const secondLng = 'fr'
-
+  // blog route for english
   const blogRoutes = allBlogs
     .filter((p) => p.language === fallbackLng)
     .map((post) => ({
       url: `${siteUrl}/${fallbackLng}/${post.path}`,
       lastModified: post.lastmod || post.date,
     }))
-
+  // blog route for french (or your own second language)
   const secondBlogRoutes = allBlogs
     .filter((p) => p.language === secondLng)
     .map((post) => ({
       url: `${siteUrl}/${secondLng}/${post.path}`,
       lastModified: post.lastmod || post.date,
     }))
-
+  // all routes for english
   const routes = ['', 'blog', 'projects', 'tags', 'about'].map((route) => ({
     url: `${siteUrl}/${fallbackLng}/${route}`,
     lastModified: new Date().toISOString().split('T')[0],
   }))
-
+  // all routes for french (or your own second language)
   const secondRoutes = ['', 'blog', 'projects', 'tags', 'about'].map((route) => ({
     url: `${siteUrl}/${secondLng}/${route}`,
     lastModified: new Date().toISOString().split('T')[0],
