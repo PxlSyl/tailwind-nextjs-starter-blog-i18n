1a37838c82b4dee49fe24f61644c8702a2e7959e
PxlSyl <SMCBeatz@protonmail.com.com>
2023-11-15 15:40:34 +0100

    fix robots.txt

--- app/[locale]/sitemap.ts
+++ app/[locale]/sitemap.ts
@@ -1,16 +1,22 @@
 import { MetadataRoute } from 'next'
 import { allBlogs } from 'contentlayer/generated'
 import siteMetadata from '@/data/siteMetadata'
+import { locales, fallbackLng } from './i18n/settings'
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const siteUrl = siteMetadata.siteUrl
+
+  const generateLocalizedUrl = (path: string, locale: string): string => {
+    return locale === fallbackLng ? `${siteUrl}/${path}` : `${siteUrl}/${locale}/${path}`
+  }
+
   const blogRoutes = allBlogs.map((post) => ({
-    url: `${siteUrl}/${post.path}`,
+    url: generateLocalizedUrl(post.path, post.language),
     lastModified: post.lastmod || post.date,
   }))
 
   const routes = ['', 'blog', 'projects', 'tags'].map((route) => ({
-    url: `${siteUrl}/${route}`,
+    url: generateLocalizedUrl(route, fallbackLng),
     lastModified: new Date().toISOString().split('T')[0],
   }))
 
