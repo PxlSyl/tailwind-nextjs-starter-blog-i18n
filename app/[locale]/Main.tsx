ca6b8882f2f2aa5f84de76b799dc544f2e943277
PxlSyl <SMCBeatz@protonmail.com.com>
2023-11-16 00:51:05 +0100

    fix draft and max_display/locale features

--- app/[locale]/Main.tsx
+++ app/[locale]/Main.tsx
@@ -12,6 +12,7 @@ interface Post {
   summary?: string | undefined
   tags: string[]
   language: string
+  draft?: boolean
 }
 
 interface HomeProps {
@@ -34,56 +35,59 @@ export default async function Home({ posts, params: { locale } }: HomeProps) {
         </div>
         <ul className="divide-y divide-gray-200 dark:divide-gray-700">
           {!posts.length && t('noposts')}
-          {posts.slice(0, MAX_DISPLAY).map((post) => {
-            const { slug, date, title, summary, tags, language } = post
-            if (language === locale) {
-              return (
-                <li key={slug} className="py-12">
-                  <article>
-                    <div className="space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0">
-                      <dl>
-                        <dt className="sr-only">{t('pub')}</dt>
-                        <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
-                          <time dateTime={date}>{formatDate(date, locale)}</time>
-                        </dd>
-                      </dl>
-                      <div className="space-y-5 xl:col-span-3">
-                        <div className="space-y-6">
-                          <div>
-                            <h2 className="text-2xl font-bold leading-8 tracking-tight">
-                              <Link
-                                href={`/${locale}/blog/${slug}`}
-                                className="text-gray-900 dark:text-gray-100"
-                              >
-                                {title}
-                              </Link>
-                            </h2>
-                            <div className="flex flex-wrap">
-                              {tags.map((tag: string) => (
-                                <Tag key={tag} text={tag} params={{ locale: locale }} />
-                              ))}
+          {posts
+            .filter((p) => p.draft === false && p.language === locale)
+            .slice(0, MAX_DISPLAY)
+            .map((post) => {
+              const { slug, date, title, summary, tags, language } = post
+              if (language === locale) {
+                return (
+                  <li key={slug} className="py-12">
+                    <article>
+                      <div className="space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0">
+                        <dl>
+                          <dt className="sr-only">{t('pub')}</dt>
+                          <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
+                            <time dateTime={date}>{formatDate(date, locale)}</time>
+                          </dd>
+                        </dl>
+                        <div className="space-y-5 xl:col-span-3">
+                          <div className="space-y-6">
+                            <div>
+                              <h2 className="text-2xl font-bold leading-8 tracking-tight">
+                                <Link
+                                  href={`/${locale}/blog/${slug}`}
+                                  className="text-gray-900 dark:text-gray-100"
+                                >
+                                  {title}
+                                </Link>
+                              </h2>
+                              <div className="flex flex-wrap">
+                                {tags.map((tag: string) => (
+                                  <Tag key={tag} text={tag} params={{ locale: locale }} />
+                                ))}
+                              </div>
+                            </div>
+                            <div className="prose max-w-none text-gray-500 dark:text-gray-400">
+                              {summary}
                             </div>
                           </div>
-                          <div className="prose max-w-none text-gray-500 dark:text-gray-400">
-                            {summary}
+                          <div className="text-base font-medium leading-6">
+                            <Link
+                              href={`/${locale}/blog/${slug}`}
+                              className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+                              aria-label={`${t('more')}"${title}"`}
+                            >
+                              {t('more')} &rarr;
+                            </Link>
                           </div>
                         </div>
-                        <div className="text-base font-medium leading-6">
-                          <Link
-                            href={`/${locale}/blog/${slug}`}
-                            className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
-                            aria-label={`${t('more')}"${title}"`}
-                          >
-                            {t('more')} &rarr;
-                          </Link>
-                        </div>
                       </div>
-                    </div>
-                  </article>
-                </li>
-              )
-            }
-          })}
+                    </article>
+                  </li>
+                )
+              }
+            })}
         </ul>
       </div>
       {posts.length > MAX_DISPLAY && (
