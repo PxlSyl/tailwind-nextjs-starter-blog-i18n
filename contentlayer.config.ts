a385c3106e9ce9105382f14a2f0b4db8da1bde1c
PxlSyl <SMCBeatz@protonmail.com.com>
2023-11-17 22:41:29 +0100

    fix

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -161,9 +161,9 @@ export default makeSource({
       rehypePresetMinify,
     ],
   },
-  /* onSuccess: async (importData) => {
+  onSuccess: async (importData) => {
     const { allBlogs } = await importData()
     createTagCount(allBlogs)
     createSearchIndex(allBlogs)
-  },*/
+  },
 })
