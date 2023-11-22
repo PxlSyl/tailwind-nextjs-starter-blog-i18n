cf8b27eb1474758dae83085291a70832760043f5
PxlSyl <SMCBeatz@protonmail.com.com>
2023-11-22 12:54:24 +0100

    fix

--- components/util/useOuterClick.ts
+++ components/util/useOuterClick.ts
@@ -1,4 +1,4 @@
-import React, { RefObject, useEffect } from 'react'
+import { RefObject, useEffect } from 'react'
 
 export function useOuterClick(dom: RefObject<HTMLElement>, cb: () => void): void {
   useEffect(() => {
