fa980a477da1a0ab60c034667444673839ea0da6
PxlSyl <SMCBeatz@protonmail.com.com>
2023-12-01 02:21:04 +0100

    up

--- layouts/PostSimple.tsx
+++ layouts/PostSimple.tsx
@@ -26,7 +26,7 @@ export default async function PostLayout({
   children,
   params: { locale },
 }: LayoutProps) {
-  const { path, slug, date, title, language } = content
+  const { slug, date, title, language } = content
   const { t } = await createTranslation(locale, 'home')
   return (
     <SectionContainer>
