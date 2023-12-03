2b25baf547caf4c5c5279d78cefbc41ce0ad3a57
PxlSyl <SMCBeatz@protonmail.com.com>
2023-12-03 15:15:40 +0100

    will fix not found translations later

--- app/[locale]/not-found.tsx
+++ app/[locale]/not-found.tsx
@@ -1,13 +1,6 @@
-'use client'
-
 import Link from '@/components/Link'
-import { useTranslation } from 'app/[locale]/i18n/client'
-import type { LocaleTypes } from 'app/[locale]/i18n/settings'
-import { useParams } from 'next/navigation'
 
 export default function NotFound() {
-  const locale = useParams()?.locale as LocaleTypes
-  const { t } = useTranslation(locale, '')
   return (
     <div className="flex flex-col items-start justify-start md:mt-24 md:flex-row md:items-center md:justify-center md:space-x-6">
       <div className="space-x-2 pb-8 pt-6 md:space-y-5">
@@ -16,13 +9,15 @@ export default function NotFound() {
         </h1>
       </div>
       <div className="max-w-md">
-        <p className="mb-4 text-xl font-bold leading-normal md:text-2xl">{t('firsttext')}</p>
-        <p className="mb-8">{t('secondtext')}</p>
+        <p className="mb-4 text-xl font-bold leading-normal md:text-2xl">
+          Sorry we couldn't find this page.
+        </p>
+        <p className="mb-8">But dont worry, you can find plenty of other things on our homepage.</p>
         <Link
           href="/"
           className="focus:shadow-outline-blue inline rounded-lg border border-transparent bg-blue-600 px-4 py-2 text-sm font-medium leading-5 text-white shadow transition-colors duration-150 hover:bg-blue-700 focus:outline-none dark:hover:bg-blue-500"
         >
-          {t('back')}
+          Back to homepage
         </Link>
       </div>
     </div>
