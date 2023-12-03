c08e45825ef55ac2c47db4de9d79f762b37b57b1
PxlSyl <SMCBeatz@protonmail.com.com>
2023-12-03 15:13:31 +0100

    framer motion smooth page transitions

---
+++ app/[locale]/template.tsx
@@ -0,0 +1,20 @@
+'use client'
+import { motion } from 'framer-motion'
+
+const variants = {
+  hidden: { opacity: 0, x: -50, y: 0 },
+  enter: { opacity: 1, x: 0, y: 0 },
+}
+
+export default function Template({ children }: { children: React.ReactNode }) {
+  return (
+    <motion.main
+      variants={variants}
+      initial="hidden"
+      animate="enter"
+      transition={{ type: 'linear' }}
+    >
+      {children}
+    </motion.main>
+  )
+}
