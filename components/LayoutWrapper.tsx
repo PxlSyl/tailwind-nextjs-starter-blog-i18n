476bc7237adf695f2d743b036260c538f5956e27
PxlSyl <SMCBeatz@protonmail.com.com>
2023-12-06 10:41:12 +0100

    fix

--- components/LayoutWrapper.tsx
+++ components/LayoutWrapper.tsx
@@ -3,18 +3,16 @@ import SectionContainer from './SectionContainer'
 import Footer from './Footer'
 import { ReactNode } from 'react'
 import Header from './Header'
-import { LocaleTypes } from 'app/[locale]/i18n/settings'
 
 interface Props {
   children: ReactNode
-  params: { locale: LocaleTypes }
 }
 
 const inter = Inter({
   subsets: ['latin'],
 })
 
-const LayoutWrapper = ({ children, params: { locale } }: Props) => {
+const LayoutWrapper = ({ children }: Props) => {
   return (
     <SectionContainer>
       <div className={`${inter.className} flex h-screen flex-col justify-between font-sans`}>
