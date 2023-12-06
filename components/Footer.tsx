26bd570c88372179ce9c7feabc549765b3932251
PxlSyl <SMCBeatz@protonmail.com.com>
2023-12-06 08:45:40 +0100

    fix icons alignment

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -27,18 +27,30 @@ export default function Footer() {
       <footer>
         <div className="mt-16 flex flex-col items-center">
           <div className="mb-3 flex space-x-4">
-            {siteMetadata.formspree === false ? (
-              <SocialIcon kind="mail" href={`mailto:${siteMetadata.email}`} />
-            ) : (
-              <button onClick={ContactClick}>
-                <SocialIcon kind="mail" />
-              </button>
-            )}
-            <SocialIcon kind="github" href={siteMetadata.github} size={6} />
-            <SocialIcon kind="facebook" href={siteMetadata.facebook} size={6} />
-            <SocialIcon kind="youtube" href={siteMetadata.youtube} size={6} />
-            <SocialIcon kind="linkedin" href={siteMetadata.linkedin} size={6} />
-            <SocialIcon kind="twitter" href={siteMetadata.twitter} size={6} />
+            <div className="flex items-center">
+              {siteMetadata.formspree === false ? (
+                <SocialIcon kind="mail" href={`mailto:${siteMetadata.email}`} />
+              ) : (
+                <button className="flex items-center focus:outline-none" onClick={ContactClick}>
+                  <SocialIcon kind="mail" />
+                </button>
+              )}
+            </div>
+            <div className="flex items-center">
+              <SocialIcon kind="github" href={siteMetadata.github} size={6} />
+            </div>
+            <div className="flex items-center">
+              <SocialIcon kind="facebook" href={siteMetadata.facebook} size={6} />
+            </div>
+            <div className="flex items-center">
+              <SocialIcon kind="youtube" href={siteMetadata.youtube} size={6} />
+            </div>
+            <div className="flex items-center">
+              <SocialIcon kind="linkedin" href={siteMetadata.linkedin} size={6} />
+            </div>
+            <div className="flex items-center">
+              <SocialIcon kind="twitter" href={siteMetadata.twitter} size={6} />
+            </div>
           </div>
           <div className="mb-2 flex space-x-2 text-sm text-gray-500 dark:text-gray-400">
             <div>{siteMetadata.author}</div>
