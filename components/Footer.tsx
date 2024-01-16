dfa32f05a80220c587cd5b951399626abdf1af10
PxlSyl <SMCBeatz@protonmail.com.com>
2024-01-16 07:16:56 +0100

    up footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -61,7 +61,9 @@ export default function Footer() {
             <Link href="/">{siteMetadata.title}</Link>
           </div>
           <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
-            <Link href="https://github.com/timlrx/tailwind-nextjs-starter-blog">{t('theme')}</Link>
+            <Link href="https://github.com/PxlSyl/tailwind-nextjs-starter-blog-i18n">
+              {t('theme')}
+            </Link>
           </div>
         </div>
       </footer>
