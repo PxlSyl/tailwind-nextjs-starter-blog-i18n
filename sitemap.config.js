6ea5f13320b1d15944f23dcc98ff3c7a0bfac256
PxlSyl <SMCBeatz@protonmail.com.com>
2024-04-30 14:42:11 +0200

    sitemap

--- sitemap.config.js
+++ sitemap.config.js
@@ -1,5 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://tailwind-nextjs-starter-blog-i18n.vercel.app',
+    siteUrl: process.env.SITE_URL || 'https://tailwind-nextjs-starter-blog-i18n.vercel.app',
     generateRobotsTxt: true,
     sitemapSize: 7000,
     outDir: '. /public' }
