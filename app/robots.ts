5d8d2a34a7b99676c1bb35ed2c39d857efd32dfa
PxlSyl <SMCBeatz@protonmail.com.com>
2024-05-25 22:57:08 +0200

    fix robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 import siteMetadata from '@/data/siteMetadata';
 
-export function GET() {
+export default function GET() {
   const robotsContent = `User-agent: *
 Allow: /
 Sitemap: ${siteMetadata.siteUrl}/sitemap.xml
