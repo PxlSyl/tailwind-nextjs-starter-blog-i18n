eda039c57d493348ce202bf05f31a6202ae2bcd8
PxlSyl <SMCBeatz@protonmail.com.com>
2024-05-26 15:19:11 +0200

    optimize

--- layouts/FeaturedLayout.tsx
+++ layouts/FeaturedLayout.tsx
@@ -26,7 +26,7 @@ export default async function FeaturedLayout({ posts, params: { locale } }: Home
   const { t } = await createTranslation(locale, 'home')
   return (
     <>
-      <div className="divide-y divide-gray-200 border-b border-gray-200 dark:divide-gray-700 dark:border-gray-700">
+      <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pb-8 pt-6 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-heading-400 dark:text-heading-400 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
             {t('featured')}
