69ef007119462e1e2c6b843a27abf274533236a0
PxlSyl <SMCBeatz@protonmail.com.com>
2024-05-26 15:30:53 +0200

    optimize

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -26,10 +26,10 @@ const siteMetadata = {
     // If you want to use an analytics provider you have to add it to the
     // content security policy in the `next.config.js` file.
     // supports Plausible, Simple Analytics, Umami, Posthog or Google Analytics.
-    umamiAnalytics: {
+    /*umamiAnalytics: {
       // We use an env variable for this site to avoid other users cloning our analytics ID
       umamiWebsiteId: process.env.NEXT_UMAMI_ID, // e.g. 123e4567-e89b-12d3-a456-426614174000
-    },
+    },*/
     // plausibleAnalytics: {
     //   plausibleDataDomain: '', // e.g. tailwind-nextjs-starter-blog.vercel.app
     // },
