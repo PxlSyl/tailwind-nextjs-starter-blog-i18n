9b2aaca082c1c09cefd7aaac7449e1375fef2319
PxlSyl <SMCBeatz@protonmail.com.com>
2024-05-26 23:05:06 +0200

    fix

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -153,7 +153,7 @@ export const Blog = defineDocumentType(() => ({
         dateModified: doc.lastmod || doc.date,
         description: doc.summary,
         image: doc.images ? doc.images[0] : siteMetadata.socialBanner,
-        url: `${siteMetadata.siteUrl}/${doc._raw.flattenedPath}`,
+        url: `${siteMetadata.siteUrl}/${doc.language}/blog/${doc.slug}`,
       }),
     },
   },
