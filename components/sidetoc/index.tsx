f77fd3ae924cc63d037546e2cadd3f33084f442c
PxlSyl <SMCBeatz@protonmail.com.com>
2024-05-30 21:36:27 +0200

    fix & destructure sidetoc UI

--- components/sidetoc/index.tsx
+++ components/sidetoc/index.tsx
@@ -19,17 +19,21 @@ const Sidetoc = ({ toc }: SidetocProps) => {
   const { t } = useTranslation(locale, 'common')
   const { sidebarOpen, toggleSidebar, closeSidebar } = useSidebarStore()
   const menubarRef = useRef<HTMLDivElement>(null)
-  const [mounted, setMounted] = useState(false)
+  const [isMounted, setIsMounted] = useState(false)
 
   useOuterClick(menubarRef, closeSidebar)
 
   useEffect(() => {
-    setMounted(true)
+    setIsMounted(true)
   }, [])
 
+  if (!isMounted) {
+    return null
+  }
+
   return (
-    mounted && (
-      <div ref={menubarRef} className={`${mounted? '' : "fixed left-0 top-0 z-50 hidden h-screen md:flex"}`}>
+    <div ref={menubarRef}>
+      <div className="fixed left-0 top-0 z-50 hidden h-screen md:flex">
         <div
           className={`transform transition-transform duration-300 ease-in-out ${
             sidebarOpen ? 'translate-x-0' : '-translate-x-full'
@@ -38,24 +42,25 @@ const Sidetoc = ({ toc }: SidetocProps) => {
           <div className="mt-20">
             <div className="text-xl font-bold text-heading-400">{t('sidetoc')}</div>
             <div className="my-auto mt-5 overflow-y-auto text-black dark:text-white">
-              <TOCInline toc={toc} ulClassName={`${mounted? 'space-y-2 overflow-y-auto my-auto text-primary-500' : 'hidden'}`}/>
+              <TOCInline
+                toc={toc}
+                ulClassName="space-y-2 overflow-y-auto my-auto text-primary-500"
+              />
             </div>
           </div>
         </div>
-        <div className="fixed bottom-8 left-6 z-50">
-          <button
-            onClick={toggleSidebar}
-            className="rounded-full bg-gray-200 p-2 text-gray-500 opacity-100 transition-colors hover:bg-gray-300 dark:bg-gray-700 dark:text-gray-400 dark:hover:bg-gray-600"
-          >
-            <ArrowRightIcon
-              className={`h-5 w-5 transform transition-transform ${
-                sidebarOpen ? 'rotate-180' : ''
-              }`}
-            />
-          </button>
-        </div>
       </div>
-    )
+      <div className="fixed bottom-8 left-6 z-50">
+        <button
+          onClick={toggleSidebar}
+          className="rounded-full bg-gray-200 p-2 text-gray-500 opacity-100 transition-colors hover:bg-gray-300 dark:bg-gray-700 dark:text-gray-400 dark:hover:bg-gray-600"
+        >
+          <ArrowRightIcon
+            className={`h-5 w-5 transform transition-transform ${sidebarOpen ? 'rotate-180' : ''}`}
+          />
+        </button>
+      </div>
+    </div>
   )
 }
 
