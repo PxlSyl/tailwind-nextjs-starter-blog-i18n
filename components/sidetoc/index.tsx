50da56aaf172084e21392cdf3cf3f276cd6555d8
PxlSyl <SMCBeatz@protonmail.com.com>
2024-05-30 15:43:21 +0200

    optimize

--- components/sidetoc/index.tsx
+++ components/sidetoc/index.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import TOCInline from 'pliny/ui/TOCInline'
 import { useTranslation } from 'app/[locale]/i18n/client'
 import { LocaleTypes } from 'app/[locale]/i18n/settings'
@@ -19,33 +19,43 @@ const Sidetoc = ({ toc }: SidetocProps) => {
   const { t } = useTranslation(locale, 'common')
   const { sidebarOpen, toggleSidebar, closeSidebar } = useSidebarStore()
   const menubarRef = useRef<HTMLDivElement>(null)
+  const [mounted, setMounted] = useState(false)
+
   useOuterClick(menubarRef, closeSidebar)
 
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
   return (
-    <div ref={menubarRef} className="fixed left-0 top-0 z-50 hidden h-screen md:flex">
-      <div
-        className={`transform transition-transform duration-300 ease-in-out ${
-          sidebarOpen ? 'translate-x-0' : '-translate-x-full'
-        } fixed left-0 top-0 z-50 flex h-screen w-64 flex-col bg-gray-100 px-2 py-4 dark:bg-gray-800`}
-      >
-        <div className="mt-20">
-          <div className="text-xl font-bold text-heading-400">{t('sidetoc')}</div>
-          <div className="my-auto mt-5 overflow-y-auto text-black dark:text-white">
-            <TOCInline toc={toc} />
+    mounted && (
+      <div ref={menubarRef} className="fixed left-0 top-0 z-50 hidden h-screen md:flex">
+        <div
+          className={`transform transition-transform duration-300 ease-in-out ${
+            sidebarOpen ? 'translate-x-0' : '-translate-x-full'
+          } fixed left-0 top-0 z-50 flex h-screen w-64 flex-col bg-gray-100 px-2 py-4 dark:bg-gray-800`}
+        >
+          <div className="mt-20">
+            <div className="text-xl font-bold text-heading-400">{t('sidetoc')}</div>
+            <div className="my-auto mt-5 overflow-y-auto text-black dark:text-white">
+              <TOCInline toc={toc} />
+            </div>
           </div>
         </div>
+        <div className="fixed bottom-8 left-6 z-50">
+          <button
+            onClick={toggleSidebar}
+            className="rounded-full bg-gray-200 p-2 text-gray-500 opacity-100 transition-colors hover:bg-gray-300 dark:bg-gray-700 dark:text-gray-400 dark:hover:bg-gray-600"
+          >
+            <ArrowRightIcon
+              className={`h-5 w-5 transform transition-transform ${
+                sidebarOpen ? 'rotate-180' : ''
+              }`}
+            />
+          </button>
+        </div>
       </div>
-      <div className="fixed bottom-8 left-6 z-50">
-        <button
-          onClick={toggleSidebar}
-          className="rounded-full bg-gray-200 p-2 text-gray-500 opacity-100 transition-colors hover:bg-gray-300 dark:bg-gray-700 dark:text-gray-400 dark:hover:bg-gray-600"
-        >
-          <ArrowRightIcon
-            className={`h-5 w-5 transform transition-transform ${sidebarOpen ? 'rotate-180' : ''}`}
-          />
-        </button>
-      </div>
-    </div>
+    )
   )
 }
 
