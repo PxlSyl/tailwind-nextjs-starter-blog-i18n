ad2114ca94e1966bd390d546e8bfc2d4fabd5bfa
PxlSyl <SMCBeatz@protonmail.com.com>
2024-06-18 14:33:19 +0200

    fix not found handling

--- middleware.ts
+++ middleware.ts
@@ -3,13 +3,19 @@ import { locales } from 'app/[locale]/i18n/settings'
 import { fallbackLng } from 'app/[locale]/i18n/locales'
 
 export function middleware(request: NextRequest) {
-  // Check if there is any supported locale in the pathname
   const pathname = request.nextUrl.pathname
+  const pathnameIsMissingLocale = locales.every(
+    (locale) => !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`
+  )
 
-  // Check if the default locale is in the pathname
-  if (pathname.startsWith(`/${fallbackLng}/`) || pathname === `/${fallbackLng}`) {
-    // e.g. incoming request is /en/about
-    // The new URL is now /about
+  if (pathnameIsMissingLocale) {
+    return NextResponse.rewrite(new URL(`/${fallbackLng}${pathname}`, request.url))
+  }
+
+  // Check if there is any supported locale in the pathname
+  const isDefaultLocalePath = pathname.startsWith(`/${fallbackLng}/`) || pathname === `/${fallbackLng}`
+
+  if (isDefaultLocalePath) {
     return NextResponse.redirect(
       new URL(
         pathname.replace(`/${fallbackLng}`, pathname === `/${fallbackLng}` ? '/' : ''),
@@ -18,23 +24,24 @@ export function middleware(request: NextRequest) {
     )
   }
 
-  const pathnameIsMissingLocale = locales.every(
-    (locale) => !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`
-  )
+  // Ensure the root paths for locales (e.g., /fr, /en) are valid and do not return not found
+  const isLocaleRootPath = locales.some(locale => pathname === `/${locale}`)
 
-  if (pathnameIsMissingLocale) {
-    // We are on the default locale
-    // Rewrite so Next.js understands
-
-    // e.g. incoming request is /about
-    // Tell Next.js it should pretend it's /en/about
-    return NextResponse.rewrite(new URL(`/${fallbackLng}${pathname}`, request.url))
+  if (isLocaleRootPath) {
+    return NextResponse.next()
   }
+
+  // Check if the path is unmatched (i.e., not found)
+  const isUnmatchedPath = !locales.some(locale => pathname.startsWith(`/${locale}/`))
+
+  if (isUnmatchedPath) {
+    // Rewrite to the custom not found page
+    return NextResponse.rewrite(new URL(`/${fallbackLng}/not-found`, request.url))
+  }
+
+  return NextResponse.next()
 }
 
 export const config = {
-  // Do not run the middleware on the following paths
-  // prettier-ignore
-  matcher:
-  '/((?!api|static|track|data|css|scripts|.*\\..*|_next).*|sitemap.xml)',
+  matcher: '/((?!api|static|track|data|css|scripts|.*\\..*|_next).*|sitemap.xml)',
 }
