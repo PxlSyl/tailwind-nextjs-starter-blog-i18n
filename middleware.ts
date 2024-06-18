4960e9f05f1479cafce494ddb964c102d5265200
PxlSyl <SMCBeatz@protonmail.com.com>
2024-06-18 15:59:10 +0200

    undo

--- middleware.ts
+++ middleware.ts
@@ -1,20 +1,15 @@
 import { NextResponse, NextRequest } from 'next/server'
 import { locales } from 'app/[locale]/i18n/settings'
-import { fallbackLng, secondLng } from 'app/[locale]/i18n/locales'
+import { fallbackLng } from 'app/[locale]/i18n/locales'
 
 export function middleware(request: NextRequest) {
+  // Check if there is any supported locale in the pathname
   const pathname = request.nextUrl.pathname
-  const url = request.nextUrl
 
-  // Ensure the root paths for locales (e.g., /fr, /en) are valid and do not return not found
-  if (locales.some(locale => pathname === `/${locale}`)) {
-    return NextResponse.next()
-  }
-
-  const isLocalePath = locales.some(locale => pathname.startsWith(`/${locale}/`))
-  const isDefaultLocalePath = pathname.startsWith(`/${fallbackLng}/`) || pathname === `/${fallbackLng}`
-
-  if (isDefaultLocalePath) {
+  // Check if the default locale is in the pathname
+  if (pathname.startsWith(`/${fallbackLng}/`) || pathname === `/${fallbackLng}`) {
+    // e.g. incoming request is /en/about
+    // The new URL is now /about
     return NextResponse.redirect(
       new URL(
         pathname.replace(`/${fallbackLng}`, pathname === `/${fallbackLng}` ? '/' : ''),
@@ -23,19 +18,23 @@ export function middleware(request: NextRequest) {
     )
   }
 
-  if (pathname === '/') {
-    // If the root path is requested, do not rewrite it, just proceed
-    return NextResponse.next()
-  }
+  const pathnameIsMissingLocale = locales.every(
+    (locale) => !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`
+  )
 
-  if (!isLocalePath) {
-    // Rewrite paths that are missing a locale
+  if (pathnameIsMissingLocale) {
+    // We are on the default locale
+    // Rewrite so Next.js understands
+
+    // e.g. incoming request is /about
+    // Tell Next.js it should pretend it's /en/about
     return NextResponse.rewrite(new URL(`/${fallbackLng}${pathname}`, request.url))
   }
-
-  return NextResponse.next()
 }
 
 export const config = {
-  matcher: '/((?!api|static|track|data|css|scripts|.*\\..*|_next).*|sitemap.xml)',
+  // Do not run the middleware on the following paths
+  // prettier-ignore
+  matcher:
+  '/((?!api|static|track|data|css|scripts|.*\\..*|_next).*|sitemap.xml)',
 }
