04ddd71f7a84662cec6aefd10aec438f608f2008
PxlSyl <SMCBeatz@protonmail.com.com>
2024-12-16 17:08:13 +0100

    fix

--- app/[locale]/blog/page.tsx
+++ app/[locale]/blog/page.tsx
@@ -6,20 +6,26 @@ import { genPageMetadata } from 'app/[locale]/seo'
 import { createTranslation } from '../i18n/server'
 import { LocaleTypes } from '../i18n/settings'
 
-type BlogPageProps = {
-  params: { locale: LocaleTypes }
+interface PageProps {
+  params: Promise<{
+    locale: LocaleTypes
+  }>
 }
 
-export async function generateMetadata({ params }: BlogPageProps): Promise<Metadata> {
-  const locale = (await params).locale
+export async function generateMetadata({ 
+  params 
+}: PageProps): Promise<Metadata> {
+  const { locale } = await params
   return genPageMetadata({
     title: 'Blog',
     params: { locale },
   })
 }
 
-export default async function BlogPage({ params }: BlogPageProps) {
-  const locale = (await params).locale
+export default async function BlogPage({ 
+  params 
+}: PageProps) {
+  const { locale } = await params
   const { t } = await createTranslation(locale, 'home')
   const posts = allCoreContent(sortPosts(allBlogs))
   const filteredPosts = posts.filter((post) => post.language === locale)
